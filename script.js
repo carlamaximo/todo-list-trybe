f2c7f3538fec881d2f90f860578120d7a13d1b1d
Carla <carladmaximo@gmail.com>
2021-07-09 22:03:38 -0300

    Requiriment 09

--- script.js
+++ script.js
@@ -13,12 +13,22 @@ function addText() {
     listText.appendChild(listItem);
     inputText.value = "" ;
 }
-// Requirements 07 and 08
+// Requirements 07, 08 and 09
 function changeColor(listText) {
   let arrayLi = document.getElementsByTagName('li');
   for(let i = 0; i < arrayLi.length; i += 1) {
     arrayLi[i].style.backgroundColor = "white";
+    arrayLi[i].style.textDecoration = 'none'
   }
   listText.target.style.backgroundColor = 'rgb(128, 128, 128)';
+  listText.target.classList.add('completed');
+  listText.target.style.textDecoration = 'line-through';
 }
 listText.addEventListener("click", changeColor);
+
+function removeLineThrough(listText) {
+  listText.target.classList.remove('completed');
+  listText.target.style.textDecoration = 'line-through solid rgb(0, 0, 0)';
+}
+
+listText.addEventListener("dblclick", removeLineThrough);
