85ea4b2ab0f9fbbae06450d856323769a27213ac
Carla <carladmaximo@gmail.com>
2021-07-12 22:30:53 -0300

    Reuirement 12

--- script.js
+++ script.js
@@ -4,32 +4,30 @@ const btnAddText = document.querySelector('#criar-tarefa');
 const listText = document.getElementById('lista-tarefas');
 
 function addText() {
-    const inputValue = inputText.value;
-    const listItem = document.createElement('li');
-    listItem.innerText = inputValue;
-    listItem.classList.add('item-lista-tarefas');
-    listText.appendChild(listItem);
-    inputText.value = '';
-  }
+  const inputValue = inputText.value;
+  const listItem = document.createElement('li');
+  listItem.innerText = inputValue;
+  listItem.classList.add('item-lista-tarefas');
+  listText.appendChild(listItem);
+  inputText.value = '';
+}
 btnAddText.addEventListener('click', addText);
 
 // Requirements 07, 08
 function changeColor(listText) {
   let arrayLi = document.getElementsByTagName('li');
-  for(let i = 0; i < arrayLi.length; i += 1) {
+  for (let i = 0; i < arrayLi.length; i += 1) {
     arrayLi[i].style.backgroundColor = 'white';
-    // arrayLi[i].style.textDecoration = 'none'
     listText.target.style.backgroundColor = 'rgb(128, 128, 128)';
   }
-  //listText.target.style.textDecoration = 'line-through';
 }
+
 listText.addEventListener('click', changeColor);
 
 // Requirement 09
 function addLineThrough(event) {
   if (event.target.classList.contains('completed')) {
   event.target.classList.remove('completed');
-  //listText.addEventListener('dblclick', addLineThrough);
   } else {
   event.target.classList.add('completed');
   }
@@ -38,18 +36,35 @@ listText.addEventListener('dblclick', addLineThrough);
 
 // Requirement 10
 const btnClear = document.getElementById('apaga-tudo');
-function btnClearList(){
+function btnClearList() {
   listText.innerHTML = '';
 }
+
 btnClear.addEventListener('click', btnClearList);
 
 // Requirement 11
+// Sourched in https://pt.stackoverflow.com/questions/4605/remover-elemento-da-p%C3%A1gina-com-javascript
 const btnRemoveFinished = document.getElementById('remover-finalizados');
-const itensCompleted = document.getElementsByClassName('completed')
+const itensCompleted = document.getElementsByClassName('completed');
 function clearFinished() {
-  while (itensCompleted.length > 0) {
+  if (itensCompleted.length > 0) {
     itensCompleted[0].parentNode.removeChild(itensCompleted[0]);
   }
 }
 
 btnRemoveFinished.addEventListener('click', clearFinished);
+
+// Requirement 12
+// from https://pt.stackoverflow.com/questions/109935/javascript-salvar-atualizar-conteudo
+const btnSave = document.getElementById('salvar-tarefas');
+function dataSave() {
+  const lis = document.querySelector('ol').innerHTML;
+  localStorage.setItem('save', lis);
+}
+function load() {
+  if (localStorage.save !== undefined) {
+    document.querySelector('ol').innerHTML = localStorage.save;
+  }
+}
+window.onload = load();
+btnSave.addEventListener('click', dataSave);
