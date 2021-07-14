13873152bc512f2ad1013f346f6046973beecd44
Carla <carladmaximo@gmail.com>
2021-07-14 09:09:55 -0300

    Requirement 14

--- script.js
+++ script.js
@@ -19,6 +19,7 @@ function changeColor(listText) {
   for (let i = 0; i < arrayLi.length; i += 1) {
     arrayLi[i].style.backgroundColor = 'white';
     listText.target.style.backgroundColor = 'rgb(128, 128, 128)';
+    listText.target.classList.add('selected');
   }
 }
 
@@ -47,7 +48,7 @@ btnClear.addEventListener('click', btnClearList);
 const btnRemoveFinished = document.getElementById('remover-finalizados');
 const itensCompleted = document.getElementsByClassName('completed');
 function clearFinished() {
-  if (itensCompleted.length > 0) {
+  while (itensCompleted.length > 0) {
     itensCompleted[0].parentNode.removeChild(itensCompleted[0]);
   }
 }
@@ -57,14 +58,41 @@ btnRemoveFinished.addEventListener('click', clearFinished);
 // Requirement 12
 // from https://pt.stackoverflow.com/questions/109935/javascript-salvar-atualizar-conteudo
 const btnSave = document.getElementById('salvar-tarefas');
-function dataSave() {
+function dataSave() { // Função que salva o conteúdo da "li" na chave "save"
   const lis = document.querySelector('ol').innerHTML;
-  localStorage.setItem('save', lis);
+  localStorage.setItem('save', lis); // Passa o conteúdo HTML de "lis" para a chave "save"
 }
 function load() {
-  if (localStorage.save !== undefined) {
-    document.querySelector('ol').innerHTML = localStorage.save;
+  if (localStorage.save !== undefined) { // se na chave "save" do localStorage não estiver "indefinido", ou seja, se não estiver vazia
+    document.querySelector('ol').innerHTML = localStorage.save; // o conteúdo HTML de toda a OL (e suas filhas) recebe o valor da chave "save" de localStorage. Isso garante que o conteúdo salvo em localStorage seja mostrado no navegador
   }
 }
-window.onload = load();
+window.onload = load(); // Função será executada quando o navegador carregar o site
 btnSave.addEventListener('click', dataSave);
+
+// Requirement 13
+///const btnUp = document.getElementById('mover-cima');
+//const btndown = document.getElementById('mover-baixo');
+
+/*function moveUp() {
+  
+
+}
+
+function moveDown() {
+
+}*/
+
+//btnUp.addEventListener('click', moveUp);
+//btndown.addEventListener('click', moveDown);
+
+// Requirement 14
+const btnRmSelected = document.getElementById('remover-selecionado');
+
+function removeSelected() {
+  if (document.querySelectorAll('.selected').length > 0) {
+    listText.removeChild(document.querySelector('.selected'));
+  }
+}
+
+btnRmSelected.addEventListener('click', removeSelected);
